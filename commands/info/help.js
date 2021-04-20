885c04d5ea4ee1561825043fcb688860cbbd62e5
Un Sophan <82254060+Sophan-Devloper@users.noreply.github.com>
2021-04-20 14:23:19 +0700

    Discord V12

---
+++ commands/info/help.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "help",
+    aliases: ["h"],
+    description: "show all cmd!",
+    run: async(client, message) => {
+        const embed = new Discord.MessageEmbed()
+        .setThumbnail(client.user.displayAvatarURL())
+        .setTitle("Bot's ping")
+        .addField("Admin", `cleardata\nleavechannel\nleavemessage\nwelchannel\nwelmessage`, true)
+        .addField("Info", `help\ninvites\nping`, true)
+        .addField("utility", `snipe`)
+        .setColor('BLACK')
+        .setTimestamp();
+        message.channel.send(embed)
+    }
+}
---
+++ commands/info/invites.js
@@ -0,0 +1,52 @@
+const { MessageEmbed } = require('discord.js');
+module.exports = {
+    name: "invites",
+    aliases: [],
+    description: "Info How many inviter",
+    run: async(client, message) => {
+       
+    if (!message.guild.me.hasPermission("MANAGE_GUILD")) {
+        const adm = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('I need the "Manage Server" permission to use this commmad.')
+        return message.reply(adm)
+    }
+
+    if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) {
+        const adm = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('I need the "Manage Channels" permission to use this commmad.')
+        return message.reply(adm)
+    }
+
+    message.guild.fetchInvites().then((invites) => {
+        const inviteCounter = {}
+
+        invites.forEach((invite => {
+            const { uses, inviter } = invite
+            const { username, discriminator } = inviter
+
+            const name = `${inviter}`
+
+            inviteCounter[name] = (inviteCounter[name] || 0) + uses
+        }))
+
+        let replyText = new MessageEmbed()
+            .setThumbnail(message.guild.avatarURL())
+            .setTitle(`📩 Invitation Cards ${message.guild.name}`)
+            .setDescription(` \n`)
+            .setColor("BLUE")
+        const sortedInvites = Object.keys(inviteCounter).sort((a, b) => inviteCounter[b] - inviteCounter[a])
+
+        if (sortedInvites.length > 5) sortedInvites.length = 5
+        else if (sortedInvites.length > 5) sortedInvites.length = sortedInvites.length
+
+
+        for (const invite of sortedInvites) {
+            const count = inviteCounter[invite]
+            replyText.description += `\n${invite} has invited ${count} member(s).`
+        }
+        message.reply(replyText)
+    })
+    }
+}
